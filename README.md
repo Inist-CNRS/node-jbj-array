ceb0937da3abb306ce43056d20464a56e5df52fb
François Parmentier <francois.parmentier@gmail.com>
2015-11-13 18:00:13 +0100

    First complete version
    Got it from node-jbj.

--- README.md
+++ README.md
@@ -1,2 +1,288 @@
-# node-jbj-array
-JBJ array module: complex actions implying arrays
+# JBJ array module
+
+JBJ array module: complex actions implying arrays ([mapping](#mapping), [mappingVar](#mappingvar), [zip](#zip), [array2object](#array2object), [arrays2objects](#arrays2objects), [coalesce](#coalesce), [substring](#substring), [getindex](#getindex), [getindexvar](#getindexvar)).
+
+
+## Contributors
+
+  * [Nicolas Thouvenin](https://github.com/touv)
+
+## Installation
+
+```bash
+$ npm install jbj-array
+```
+
+## Usage
+
+This JBJ module cannot be used alone. JBJ has to be installed.
+
+```js
+var JBJ = require('jbj');
+JBJ.use(require('jbj-array'));
+```
+
+## Tests
+
+Use [mocha](https://github.com/visionmedia/mocha) to run the tests.
+
+```bash
+$ npm install --dev
+$ npm test
+```
+
+## Actions
+
+Once the module is declared as used for JBJ, you can use the following actions:
+
+<a id="mapping"></a>
+### mapping: object
+*module: array*
+
+Replace a value by the matching value in the object.
+
+```javascript
+{
+  "set": "one",
+  "mapping": {
+    "one": 1
+  }
+}
+// output: 1
+```
+
+```javascript
+{
+  "set": "FR",
+  "mapping": {
+    "US": "United States of America",
+    "FR": "France"
+  }
+}
+// output: "France"
+```
+
+Can also replace the values of an array with the matching values in the object.
+
+```javascript
+{
+  "set": [1, 2],
+  "mapping": ["a","b","c"]
+}
+// output: ["b","c"]
+```
+
+```javascript
+{
+  "set": ["a", "b"],
+  "mapping": {
+    "a": "Aha!",
+    "b": "Baby"
+  }
+}
+// output: ["Aha!","Baby"]
+```
+
+<a id="mappingvar"></a>
+### mappingVar: ["input","table"]
+
+*module: array*
+*alias*: combine
+
+Replace the content of the `input` variable according to the content of the `table` variable.
+
+```javascript
+var input = {
+  "arg": { "a": "Aha!", "b": "Baby"},
+  "input": "a"
+};
+var stylesheet = {
+  "mappingVar": ["input", "arg"]
+};
+var output = JBJ.renderSync(stylesheet, input);
+// output "Aha!";
+```
+
+<a id="array2object"></a>
+### array2object: [key, value]
+*module: array*
+
+Convert an array, which items have `key` and `value` properties, to an associative array (or object), which key properties are `key` values and values are `value` values.
+
+> *Note*: when the parameter is not a two items array, its default value is `["_id","value"]`.
+
+Ex:
+
+```javascript
+var stylesheet = {
+  "set": [
+    {
+      "_id": "2007",
+      "value": 538
+    }, {
+      "_id": "2008",
+      "value": 577
+    }, {
+      "_id": "2009",
+      "value": 611
+  }],
+  "array2object": true
+};
+// output = { "2007": 538, "2008": 577, "2009": 611 }
+
+var stylesheet = {
+  "set": [
+    {
+      "key": "2007",
+      "val": 538
+    }, {
+      "key": "2008",
+      "val": 577
+    }, {
+      "key": "2009",
+      "val": 611
+  }],
+  "array2object": ["key","val"]
+};
+// output = { "2007": 538, "2008": 577, "2009": 611 }
+```
+
+<a id="arrays2objects"></a>
+### arrays2objects: [key, value]
+*module: array*
+
+Convert an array of arrays (of 2 items), to an array of objects, where the first key if `key` and the second `value`). Defailt value of `key`: `_id`, default value of `value`: `value`.
+
+> *Note*: this is useful to prepare data from a CSV file to be treated with `array2object`.
+
+Ex:
+
+```javascript
+var stylesheet = {
+  "set": [ [ "Afghanistan", "AFG" ],
+           [ "Aland Islands", "ALA" ] ],
+  "arrays2objects": ["key", "val"]
+};
+// output: [ { "key": "Afghanistan", "val": "AFG"},
+//           { "key": "Aland Islands", "val": "ALA"} ]
+```
+
+```javascript
+var stylesheet = {
+  "set": [ [ "Afghanistan", "AFG" ],
+           [ "Aland Islands", "ALA" ] ],
+  "arrays2objects": true
+};
+// output: [ { "_id": "Afghanistan", "value": "AFG"},
+//           { "_id": "Aland Islands", "value": "ALA"} ]
+```
+
+<a id="zip"></a>
+### zip: ["array1","array2"]
+*module: array*
+
+Join two arrays (which elements have an `_id` and a `value` keys).
+
+```javascript
+var stylesheet = {
+  "set": {
+    "array1": [{"_id": "1", "value": 1},  {"_id": "2", "value": 2}],
+    "array2": [{"_id": "1", "value": 10}, {"_id": "2", "value": 20}]
+  },
+  "zip": [ "array1", "array2" ]
+};
+var output = JBJ.renderSync(stylesheet);
+// output: [ { _id: '1', array1: 1, array2: 10 },
+//           { _id: '2', array1: 2, array2: 20 } ]
+
+```
+
+<a id="coalesce"></a>
+### coalesce: none
+*module: array*
+
+Get the first non-undefined value
+```javascript
+    var stylesheet = {
+        "set" : [null, undefined, null, "a", "b"],
+        "coalesce": true
+    };
+    // output : "a"
+```
+
+<a id="substring"></a>
+### substring: [offset]|[offset, length]
+
+*module: array*
+*aliases : substr*
+
+```javascript
+    var stylesheet = {
+      "set"       : "20150310",
+      "substring" : [4,2]
+    };
+    // output : "03"
+```
+
+<a id="getproperty"></a>
+<a id="getindex"></a>
+### getindex: property | index
+*module: array*
+*aliases : getProperty, getproperty, getIndex*
+
+Get a property of an object, or an item of an array.
+
+```javascript
+var stylesheet = {
+  "set"        : [ "a", "b", "c" ],
+  "getindex": "2"
+};
+// output : "c"
+var stylesheet = {
+  "set"        : { "a": 0, "b": 1, "c":2 },
+  "getproperty": "b"
+};
+// output : 1
+```
+
+<a id="getpropertyvar"></a>
+<a id="getindexvar"></a>
+### getindexvar: [ arrayName | objectName , propertyName | indexName ]
+*module: array*
+*aliases : getPropertyVar, getpropertyvar, getIndexVar*
+
+Get a property of an object, or an item of an array, like [getindex](#getindex), but using variables.
+
+```javascript
+var stylesheet = {
+  "set": {
+    "i": 1,
+    "t": ["a","b","c"]
+  },
+  "getIndexVar": ["t", "i"]
+};
+// output : "b"
+var stylesheet = {
+  "set": {
+    "i" : "b",
+    "o" : { "a": 0, "b": 1, "c":2 },
+  },
+  "getPropertyVar": ["o", "i"]
+};
+// output : 1
+```
+
+
+## Examples
+
+See unit tests : https://github.com/Inist-CNRS/node-jbj-array/tree/master/test
+
+
+## Try it
+
+http://Inist-CNRS.github.io/node-jbj/
+
+
+## License
+
+[MIT](https://github.com/Inist-CNRS/node-jbj-array/blob/master/LICENSE)
---
+++ test/arrayAsync.js
@@ -0,0 +1,270 @@
+/*jshint node:true, laxcomma:true */
+/* global describe, it */
+'use strict';
+var assert = require('assert')
+  , JBJ = require('jbj');
+
+JBJ.use(require('..'));
+
+describe('asynchronous basic', function (done) {
+  var input = {
+    "a" : {
+      "b" : {
+        "c" : "value"
+      },
+      "d" : ['C', 'B', 'A'],
+      "e" : 3
+    }
+  };
+
+  it('array #1', function(done) {
+    var stylesheet = {
+      "get" : "a.b.c",
+      "mapping" : {
+        "value" : 1
+      }
+    };
+    JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output, 1);
+      done(err);
+    });
+  });
+
+  it('array #2', function(done) {
+    var stylesheet = {
+      "default" : 1,
+      "mapping" : ['a','b','c']
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, 'b');
+      done(err);
+    });
+  });
+
+
+  it('array #3', function(done) {
+    var stylesheet = {
+      "set": [1, 2],
+      "mapping": ['a','b','c']
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(JSON.stringify(output), '["b","c"]');
+      done(err);
+    });
+  });
+
+  it('array #4', function(done) {
+    var stylesheet = {
+      "set": ["a", "b"],
+      "mapping": {
+        "a": "Aha!",
+        "b": "Baby"
+      }
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(JSON.stringify(output), '["Aha!","Baby"]');
+      done(err);
+    });
+  });
+
+  it('array #5', function(done) {
+    var input = {
+      "arg": { "a": "Aha!", "b": "Baby"},
+      "input": "a"
+    };
+    var stylesheet = {
+      "mappingVar": ["input", "arg"]
+    };
+    JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output, "Aha!");
+      done(err);
+    });
+  });
+
+
+  it('array #6', function (done) {
+    var stylesheet = {
+      "set"       : "20150310",
+      "substring" : [4,2]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, "03");
+      done(err);
+    });
+  });
+
+  it('array #7', function (done) {
+    var stylesheet = {
+      "set"        : [ "a", "b", "c" ],
+      "getindex": "2"
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, "c");
+      done(err);
+    });
+  });
+
+  it('array #8', function (done) {
+    var stylesheet = {
+      "set"        : { "a": 0, "b": 1, "c":2 },
+      "getproperty": "b"
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, 1);
+      done(err);
+    });
+  });
+
+  it('array #9', function (done) {
+    var stylesheet = {
+      "set": {
+        "i": 1,
+        "t": ["a","b","c"]
+      },
+      "getIndexVar": ["t", "i"]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, "b");
+      done(err);
+    });
+  });
+
+  it('array #10', function (done) {
+    var stylesheet = {
+      "set": {
+        "i" : "b",
+        "o" : { "a": 0, "b": 1, "c":2 },
+      },
+      "getPropertyVar": ["o", "i"]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output, 1);
+      done(err);
+    });
+  });
+
+  it('array #11', function (done) {
+    var stylesheet = {
+      "set": [
+        {
+          "_id": "2007",
+          "value": 538
+        }, {
+          "_id": "2008",
+          "value": 577
+        }, {
+          "_id": "2009",
+          "value": 611
+      }],
+      "array2object": true
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output[2007], 538);
+      assert.equal(output[2008], 577);
+      assert.equal(output[2009], 611);
+      done(err);
+    });
+  });
+
+  it('array #12', function (done) {
+    var stylesheet = {
+      "set": [
+        {
+          "key": "2007",
+          "val": 538
+        }, {
+          "key": "2008",
+          "val": 577
+        }, {
+          "key": "2009",
+          "val": 611
+      }],
+      "array2object": ["key","val"]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output[2007], 538);
+      assert.equal(output[2008], 577);
+      assert.equal(output[2009], 611);
+      done(err);
+    });
+  });
+
+  it('array #13', function (done) {
+    var stylesheet = {
+      "set": [ [ "Afghanistan", "AFG" ],
+               [ "Aland Islands", "ALA" ] ],
+      "arrays2objects": true
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output[0]._id,"Afghanistan");
+      assert.equal(output[0].value, "AFG");
+      assert.equal(output[1]._id, "Aland Islands");
+      assert.equal(output[1].value, "ALA");
+      done(err);
+    });
+  });
+
+  it('array #14', function (done) {
+    var stylesheet = {
+      "set": [ [ "Afghanistan", "AFG" ],
+               [ "Aland Islands", "ALA" ] ],
+      "arrays2objects": ["key", "val"]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output[0].key,"Afghanistan");
+      assert.equal(output[0].val, "AFG");
+      assert.equal(output[1].key, "Aland Islands");
+      assert.equal(output[1].val, "ALA");
+      done(err);
+    });
+  });
+
+  it('array #15', function (done) {
+    var stylesheet = {
+      "set": {
+        "array1": [{"_id": "1", "value": 1},  {"_id": "2", "value": 2}],
+        "array2": [{"_id": "1", "value": 10}, {"_id": "2", "value": 20}]
+      },
+      "zip": [ "array1", "array2" ]
+    };
+    JBJ.render(stylesheet, function (err, output) {
+      assert.equal(output[0]._id, "1");
+      assert.equal(output[0].array1, 1);
+      assert.equal(output[0].array2, 10);
+      assert.equal(output[1]._id, "2");
+      assert.equal(output[1].array1, 2);
+      assert.equal(output[1].array2, 20);
+      done();
+    });
+  });
+
+  it('array #16', function(done) {
+    var stylesheet = {
+      "find" : ["x", "x.y", "a.b.c"],
+      "coalesce" : null
+    };
+    JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output, 'value');
+      done(err);
+    });
+  });
+
+  it('array #17', function(done) {
+    var stylesheet = {
+      "$x.y" : {
+        "find" : ["x.y", "a.b.c"],
+        "coalesce" : null,
+        "upcase" : null
+      },
+      "$x.z" : {
+        "find" : ["x.y", "a.b.c"],
+        "first" : null
+      }
+    };
+    JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.x.z, 'VALUE');
+      done(err);
+    });
+  });
+
+});
